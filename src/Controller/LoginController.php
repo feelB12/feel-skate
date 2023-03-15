dba71b4e4e1035571b41db83c74cd5d3418d2611
feelB12git config --global user.mname feelB12 <philippebidouze@gmail.com>
2023-03-15 21:25:24 +0100

    youpi login admin ok

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,33 +2,33 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     /**
-     * @Route("admin/login", name="admin_login")
-     */
+    * @Route("/login", name="login")
+    */
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
-
         $lastUsername = $authenticationUtils->getLastUsername();
+        
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
-             'error'         => $error,
-        ]);
+            'error' => $error,
+    ]); 
     }
-
     /**
-     * @Route("admin/logout", name="app_logout", methods={"GET"})
-     */
+    * @Route("/logout", name="app_logout", methods={"GET"})
+    */
     public function logout(): void
     {
         // controller can be blank: it will never be called!
         throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
+    
 }
