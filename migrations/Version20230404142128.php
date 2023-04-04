79e544bd3996c7857a54503e4adb39d4b567aed1
feelB12git config --global user.mname feelB12 <philippebidouze@gmail.com>
2023-04-04 16:22:47 +0200

    debug linedatabase

--- migrations/Version20230404142128.php
+++ migrations/Version20230404142128.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230330102352 extends AbstractMigration
+final class Version20230404142128 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20230330102352 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE club ADD user_id INT NOT NULL');
-        $this->addSql('ALTER TABLE club ADD CONSTRAINT FK_B8EE3872A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_B8EE3872A76ED395 ON club (user_id)');
+        $this->addSql('ALTER TABLE club DROP user_id');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE club DROP FOREIGN KEY FK_B8EE3872A76ED395');
-        $this->addSql('DROP INDEX IDX_B8EE3872A76ED395 ON club');
-        $this->addSql('ALTER TABLE club DROP user_id');
+        $this->addSql('ALTER TABLE club ADD user_id INT NOT NULL');
     }
 }
